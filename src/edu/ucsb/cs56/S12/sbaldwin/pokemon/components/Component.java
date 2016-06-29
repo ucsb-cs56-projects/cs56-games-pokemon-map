87031b3f20eba87fbe70b01b6ecb124588252b40
William Bennett <williambennett2014@gmail.com>
2016-06-29 13:41:36 -0700

    Minor commit

--- src/edu/ucsb/cs56/S12/sbaldwin/pokemon/components/Component.java
+++ src/edu/ucsb/cs56/S12/sbaldwin/pokemon/components/Component.java
@@ -3,5 +3,9 @@ package edu.ucsb.cs56.S12.sbaldwin.pokemon.components;
 /**
  * Created by William Bennett on 6/29/2016.
  */
-public class Component {
+
+// Component exists as a structural class
+    // It could possibly be an interface
+    // Generally, components are bags of data (state). All behavior is in systems. Thus, all their members are public
+public abstract class Component {
 }
--- src/edu/ucsb/cs56/S12/sbaldwin/pokemon/components/PositionComponent.java
+++ src/edu/ucsb/cs56/S12/sbaldwin/pokemon/components/PositionComponent.java
@@ -1,7 +1,14 @@
 package edu.ucsb.cs56.S12.sbaldwin.pokemon.components;
 
+import com.sun.javafx.geom.Vec2f;
+
+import java.awt.geom.Point2D;
+
 /**
  * Created by William Bennett on 6/29/2016.
  */
-public class PositionComponent {
+
+// Component for the position of the owner entity
+public class PositionComponent extends Component {
+    public Point2D position;
 }
