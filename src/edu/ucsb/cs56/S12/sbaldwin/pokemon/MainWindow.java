bc5531455872dc6251373a449a77847973626ed7
William Bennett <williambennett2014@gmail.com>
2016-07-01 09:32:10 -0700

    Rewrote MainWindow.java into a proper game loop. All game code goes into MainWindow.java, or is an object whose roots are in MainWindow.java

--- src/edu/ucsb/cs56/S12/sbaldwin/pokemon/MainWindow.java
+++ src/edu/ucsb/cs56/S12/sbaldwin/pokemon/MainWindow.java
@@ -1,45 +1,79 @@
 package edu.ucsb.cs56.S12.sbaldwin.pokemon;
 
+import javax.imageio.ImageIO;
+import javax.management.timer.Timer;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Time;
+import java.util.*;
+
 /** The window frame for the Pokemon map game
     @author Samuel Baldwin
     @author Marcus Liou
     @author Alec Harrell
 */
-public class MainWindow
+public class MainWindow extends JPanel
 {
-	JFrame frame;
-	public int width;
-	public int height;
+    boolean running = true;
+    int width;
+    int height;
+    static final long maxTime = 16666;
+    BufferedImage image;
+    public MainWindow(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
 
-        /** Two arg constructor
-	    @param width width of the window
-	    @param height height of the window
-	*/
-	public MainWindow(int width, int height)
-	{
-		this.width = width;
-		this.height = height;
-		
-		//Create a new JFrame, with the GamePanel located in the center region
-		frame = new JFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		GamePanel gp = new GamePanel(width, height);
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.black);
+        g.drawRect(0, 0, width, height);
+        draw(g);
+    }
 
-		frame.setSize(width, height);		
-		frame.getContentPane().add(BorderLayout.CENTER, gp);		
-		frame.setLocation(50, 50);
-		frame.setVisible(true);
-		
-		// Account for size of Border
-		Insets insets = frame.getInsets();
-		frame.setSize((width + insets.left + insets.right)+176, (height + insets.top + insets.bottom)+176);
-		//window is -176 so that it renders the entire game space
-		
-		//Start the game panel
-		gp.startLoop();
-	}
+    public void run() {
+        long currentTime = System.nanoTime();
+        long deltaT = 0;
+        init();
+        load();
+
+        while (running) {
+            update();
+            repaint();
+            deltaT = System.nanoTime() - currentTime;
+            try {
+                Thread.sleep(Math.max((maxTime - deltaT) / 1000, 0));
+            }
+            catch (InterruptedException e) {
+                System.err.println("Thread interupted");
+            }
+            currentTime = System.nanoTime();
+        }
+    }
+
+    private void init() {
+
+    }
+
+    private void load() {
+        try {
+            image = ImageIO.read(new File("assets/images/TileSet.bmp"));
+        }
+        catch (IOException e) {
+            System.err.println("Failed to load image");
+        }
+    }
+
+    private void update() {
+
+    }
+
+    private void draw(Graphics g) {
+        g.drawImage(image, 0, 0, null);
+    }
 }
