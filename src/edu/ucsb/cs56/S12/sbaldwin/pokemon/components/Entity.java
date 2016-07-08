0dd32081abeda8a2a88e1266577827f7cb32ff51
Steven Fields <smfields@umail.ucsb.edu>
2016-07-08 09:29:36 -0700

    Added Entity class to store components

---
+++ src/edu/ucsb/cs56/S12/sbaldwin/pokemon/components/Entity.java
@@ -0,0 +1,21 @@
+package edu.ucsb.cs56.S12.sbaldwin.pokemon;
+
+import java.util.ArrayList;
+
+public class Entity {
+
+    private ArrayList<Component> components;
+
+    public Entity() {
+	components = new ArrayList<Component>();
+    }
+
+    public Entity(ArrayList<Component> components) {
+	this.components = components;
+    }
+
+    public ArrayList<Component> getComponents() {
+	return components;
+    }
+    
+}
