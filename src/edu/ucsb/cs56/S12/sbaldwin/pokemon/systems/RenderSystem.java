16f7cb3feaa9e18abf89696ec5b9c41999f9fb87
William Bennett <williambennett2014@gmail.com>
2016-07-15 16:46:15 -0700

    Implemented RenderSystem

--- src/edu/ucsb/cs56/S12/sbaldwin/pokemon/systems/RenderSystem.java
+++ src/edu/ucsb/cs56/S12/sbaldwin/pokemon/systems/RenderSystem.java
@@ -1,6 +1,13 @@
 package edu.ucsb.cs56.S12.sbaldwin.pokemon.systems;
 
+import edu.ucsb.cs56.S12.sbaldwin.pokemon.Vector2;
+import edu.ucsb.cs56.S12.sbaldwin.pokemon.World;
+import edu.ucsb.cs56.S12.sbaldwin.pokemon.components.Entity;
+import edu.ucsb.cs56.S12.sbaldwin.pokemon.components.GraphicsComponent;
+import edu.ucsb.cs56.S12.sbaldwin.pokemon.components.PositionComponent;
 import edu.ucsb.cs56.S12.sbaldwin.pokemon.graphics.*;
+import javafx.geometry.Pos;
+import org.w3c.dom.css.Rect;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -14,26 +21,37 @@ import java.util.HashMap;
  * Created by William Bennett on 6/30/2016.
  */
 public class RenderSystem {
-    HashMap<String, BufferedImage> spriteSheets;
-
-    public RenderSystem() {
-        spriteSheets = new HashMap<String, BufferedImage>();
+    Rectangle cameraBounds;
+    private static int spriteResolution = 16;
+    public RenderSystem(Rectangle cameraBounds) {
+        this.cameraBounds = cameraBounds;
     }
 
-    public Texture createTexture(String spritesheet, Rectangle bounds) {
-        return new Texture(spriteSheets.get(spritesheet), bounds);
-    }
+    public void draw(SpriteBatch spriteBatch, World world) {
+        for (int i = 0; i < world.tiles.length; i++) {
+            for (int j = 0; j < world.tiles[i].length; j++) {
+                Entity e = world.tiles[i][j];
+                GraphicsComponent graphicsComponent = (GraphicsComponent)e.getComponent(GraphicsComponent.class);
+                if (graphicsComponent == null)
+                    continue;
+                PositionComponent positionComponent = (PositionComponent)e.getComponent(PositionComponent.class);
+                if (positionComponent == null)
+                    continue;
 
-    public void loadImages(String[] imageNames) {
-        for (String name : imageNames) {
-            try {
-                spriteSheets.put(name, ImageIO.read(new File(name)));
-            }
-            catch (IOException e) {
-                System.err.println("Image " + name + " not found");
-                System.err.println("At stacktrace:");
-                e.printStackTrace(System.err);
+                spriteBatch.draw(graphicsComponent.animation.currentFrame(), scale(positionComponent.position, spriteResolution));
             }
         }
+    }
+
+    private static Rectangle scale(Rectangle rect, int amount) {
+        return new Rectangle(rect.x * amount, rect.y * amount, rect.width * amount, rect.height * amount);
+    }
+
+    private static Point scale(Point point, int amount) {
+        return new Point(point.x * amount, point.y * amount);
+    }
+
+    private static Vector2 scale(Vector2 vector, int amount) {
+        return new Vector2(vector.x * amount, vector.y * amount);
     }
 }
