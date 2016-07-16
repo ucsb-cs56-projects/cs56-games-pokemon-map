43c81aa2b9c2796024102e1ad133a3b36f8aef02
Steven Fields <smfields@umail.ucsb.edu>
2016-07-15 18:45:19 -0700

    Fixed most of the textures

--- src/edu/ucsb/cs56/S12/sbaldwin/pokemon/Assets.java
+++ src/edu/ucsb/cs56/S12/sbaldwin/pokemon/Assets.java
@@ -10,45 +10,52 @@ import java.io.IOException;
 
 public class Assets {
     public static Texture
-	grass_Default,
-	grass_Wild,
-	flower,
-	flower2,
-	flower3,
-	rockGround,
-	snow,
-	bigPokemon,
-	pikachu, // TODO
-	player,  // TODO
-	pokeball,
-	pokecenter,
-	pokemart,
-	smallHouse,
-	tree,
-	mediumHouse,
-	largeHouse,
-	bigFish,
-	door,
-	bigBuildingLeft,
-	bigBuildingRight,
-	bigBuilding2Left,
-	bigBuilding2Right,
-	bigRock,
-	water, // TODO
-	rock,
-	niceTree,
-	god,
-	textBox;
+            grass_Default,
+            grass_Wild,
+            flower,
+            flower2,
+            flower3,
+            snow,
+            bigPokemon,
+            pikachu, // TODO
+            player,  // TODO
+            pokeball,
+            pokecenter,
+            pokemart,
+            smallHouse,
+            tree,
+            mediumHouse,
+            largeHouse,
+            bigFish,
+            door,
+            bigBuildingLeft,
+            bigBuildingRight,
+            bigBuilding2Left,
+            bigBuilding2Right,
+            bigRock,
+            water1, 
+            water2,
+            water3,
+            water4,
+            water5,
+            water6,
+            water7,
+            water8,
+            water9,
+            rock,
+            niceTree,
+            god,
+            textBox;
 
 
     public static void initAssets() {
-	BufferedImage TileSet_Main = addImage("images/TilesetMain.bmp");
-	BufferedImage TileSet_Characters = addImage("images/NPC_frlg.bmp");
-	BufferedImage TileSet_Pkmn = addImage("images/pkmn.bmp");
-	TileSet_Main = imageToBufferedImage(makeColorTransparent(TileSet_Main, Color.black));
-	TileSet_Characters = imageToBufferedImage(makeColorTransparent(TileSet_Characters, new Color(255, 0, 255)));
-	TileSet_Pkmn = imageToBufferedImage(makeColorTransparent(TileSet_Pkmn, Color.white));
-	
+        BufferedImage TileSet_Main = addImage("images/TilesetMain.bmp");
+        BufferedImage TileSet_Characters = addImage("images/NPC_frlg.bmp");
+        BufferedImage TileSet_Pkmn = addImage("images/pkmn.bmp");
+        TileSet_Main = imageToBufferedImage(makeColorTransparent(TileSet_Main, Color.black));
+        TileSet_Characters = imageToBufferedImage(makeColorTransparent(TileSet_Characters, new Color(255, 0, 255)));
+        TileSet_Pkmn = imageToBufferedImage(makeColorTransparent(TileSet_Pkmn, Color.white));
+
 
 
         grass_Default = new Texture(TileSet_Main, 16, 0, 16, 16);
@@ -56,35 +63,43 @@ public class Assets {
         flower = new Texture(TileSet_Main, 16 * 0, 16 * 36, 16, 16);
         flower2 = new Texture(TileSet_Main, 16 * 13, 16 * 39, 16, 16);
         flower3 = new Texture(TileSet_Main, 16 * 14, 16 * 189, 16, 16);
-        rockGround = new Texture(TileSet_Main, 16 * 6, 16 * 10, 16, 16);
         snow = new Texture(TileSet_Main, 16 * 1, 16 * 351, 16, 16);
 
-        bigPokemon = new Texture(TileSet_Main, 16 * 31, 16 * 17, 16 * 32, 16 * 20);
-        pokeball = new Texture(TileSet_Main, 16 * 5, 16 * 3, 16 * 6, 16 * 4);
-        pokecenter = new Texture(TileSet_Main, new Rectangle(0, 720, 80, 800));
-        pokemart = new Texture(TileSet_Main, new Rectangle(0, 656, 64, 720));
-        smallHouse = new Texture(TileSet_Main, 0, 1840, 80, 1888);
-        tree = new Texture(TileSet_Main, 240, 3632, 256, 3664);
-        mediumHouse = new Texture(TileSet_Main, 0, 1888, 80, 1968);
-        largeHouse = new Texture(TileSet_Main, 16, 1968, 128, 2048);
-        bigFish = new Texture(TileSet_Main, 16 * 8, 16 * 359, 16 * 4, 16 * 373);
-        door = new Texture(TileSet_Main, 16 * 8, 16 * 401, 16 * 13, 16 * 406);
-        bigBuildingLeft = new Texture(TileSet_Main, 16 * 8, 16 * 373, 16, 16);
-        bigBuildingRight = new Texture(TileSet_Main, 16 * 8, 16 * 378, 16, 16);
-        bigBuilding2Left = new Texture(TileSet_Main, 0, 16 * 469, 16, 16);
-        bigBuilding2Right = new Texture(TileSet_Main, 16, 16 * 482, 16, 16);
-        bigRock = new Texture(TileSet_Main, 160, 16 * 21, 16, 16);
-        god = new Texture(TileSet_Pkmn, 16 * 22, 16 * 24, 16, 16);
+        bigPokemon = new Texture(TileSet_Pkmn, 16 * 31, 16 * 17, 16*4, 16 * 3);
+        pokeball = new Texture(TileSet_Main, 16 * 5, 16 * 3, 16, 16);
+        pokecenter = new Texture(TileSet_Main, new Rectangle(0, 720, 80, 80));
+        pokemart = new Texture(TileSet_Main, new Rectangle(0, 656, 64, 64));
+        smallHouse = new Texture(TileSet_Main, 0, 1840, 80, 48);
+        tree = new Texture(TileSet_Main, 240, 3632, 16, 32);
+        mediumHouse = new Texture(TileSet_Main, 0, 1888, 80, 80);
+        largeHouse = new Texture(TileSet_Main, 16, 1968, 112, 80);
+        bigFish = new Texture(TileSet_Main, 16 * 8, 16 * 359, 16 * 6, 16 * 14);
+        door = new Texture(TileSet_Main, 16 * 8, 16 * 401, 16 * 5, 16 * 5);
+        bigBuildingLeft = new Texture(TileSet_Main, 16 * 8, 16 * 373, 16*7, 16*14);
+        bigBuildingRight = new Texture(TileSet_Main, 16 * 8, 16 * 387, 16*6, 16*14);
+        bigBuilding2Left = new Texture(TileSet_Main, 0, 16 * 469, 16*8, 16*13);
+        bigBuilding2Right = new Texture(TileSet_Main, 16, 16 * 482, 16*7, 16*13);
+        bigRock = new Texture(TileSet_Main, 160, 16 * 21, 48, 48);
+        god = new Texture(TileSet_Pkmn, 16 * 22, 16 * 24, 16*3, 16*4);
+        niceTree = new Texture(TileSet_Main, 192, 3696, 224 - 192, 3728 - 3696);
+        rock = new Texture(TileSet_Main, 48, 16, 16, 16);
+        textBox = new Texture(imageToBufferedImage(addImage("images/textbox.bmp")), 0, 0, 252, 47);
+        water1 = new Texture(TileSet_Main, 16*5, 16*26, 16, 16);
+        water2 = new Texture(TileSet_Main, 16*6, 16*26, 16, 16);
+        water3 = new Texture(TileSet_Main, 16*7, 16*26, 16, 16);
+        water4 = new Texture(TileSet_Main, 16*5, 16*27, 16, 16);
+        water5 = new Texture(TileSet_Main, 16*6, 16*27, 16, 16);
+        water6 = new Texture(TileSet_Main, 16*7, 16*27, 16, 16);
+        water7 = new Texture(TileSet_Main, 16*5, 16*28, 16, 16);
+        water8 = new Texture(TileSet_Main, 16*6, 16*28, 16, 16);
+        water9 = new Texture(TileSet_Main, 16*7, 16*28, 16, 16);
 
-	//textBox = new Texture(imageToBufferedImage(addImage("images/textbox.bmp")), 0, 0, 252, 47);
-	
-	
-				
-	
+
+
     }
 
     private static BufferedImage addImage(String name) {
-	BufferedImage img = null;
+        BufferedImage img = null;
         try {
             File f = new File("src/edu/ucsb/cs56/S12/sbaldwin/pokemon/" + name);
             System.out.println(f.getAbsolutePath());
