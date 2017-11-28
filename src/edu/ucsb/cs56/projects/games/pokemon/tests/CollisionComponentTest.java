a732a6acd45584db29566bacf8b9592c7a56b336
Danielle Robinson <daniellerobinson@umail.ucsb.edu>
2017-11-27 21:20:28 -0800

    Added more units tests for collision, movement, position and tile components

---
+++ src/edu/ucsb/cs56/projects/games/pokemon/tests/CollisionComponentTest.java
@@ -0,0 +1,33 @@
+package edu.ucsb.cs56.projects.games.pokemon.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import edu.ucsb.cs56.projects.games.pokemon.components.CollisionComponent;
+
+
+public class CollisionComponentTest {
+
+	@Test public void noArgConstructor() {
+		CollisionComponent C1 = new CollisionComponent();
+		Assert.assertTrue(C1.hasCollision == false);
+		Assert.assertTrue(C1.height == 0);
+		Assert.assertTrue(C1.width == 0);
+	}
+	
+	@Test public void constructorWithCollisionandIntArgs() {
+		CollisionComponent C2 = new CollisionComponent(true, 64, 32);
+		Assert.assertTrue(C2.hasCollision == true);
+		Assert.assertTrue(C2.height == 2);
+		Assert.assertTrue(C2.width == 4);
+	}
+
+	@Test public void constructorWithCollisionandRectangleArg() {
+		java.awt.Rectangle R1 = new java.awt.Rectangle(64, 64);
+		CollisionComponent C3 = new CollisionComponent(false, R1);
+		Assert.assertTrue(C3.hasCollision == false);
+		Assert.assertTrue(C3.height == 3);
+		Assert.assertTrue(C3.width == 3);	
+	}
+}
+
